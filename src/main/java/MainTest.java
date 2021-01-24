8831491f3ef0193c7f4c7641962debf43dbd916f
Biresh Patel <kumarb4jobs@gmail.com>
2021-01-24 17:28:45 +0000

    Change for Docker image!

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -2,5 +2,6 @@ public class MainTest {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Change for Docker image!");
     }
 }
